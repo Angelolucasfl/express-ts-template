a09aa5d0a48ff51d38da49a81ee464061efb0c1a
Angelo Lucas <Angelolucasfl@gmail.com>
2024-01-08 23:34:15 -0300

    Adding swagger

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 export class App {
   public app: express.Application;
 
   constructor() {
     this.app = express();
-    this.routes();
     this.config();
+    this.routes();
   }
 
   private config(): void {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cors());
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
   }
 
   routes() {
