bad20a729315af9c7f5d53ed341f38d5af63d57f
Angelo Lucas <Angelolucasfl@gmail.com>
2024-01-16 16:46:52 -0300

    identation problems

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,30 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swagger.json";
 
 export class App {
-  public app: express.Application;
+    public app: express.Application;
 
-  constructor() {
-    this.app = express();
-    this.config();
-    this.routes();
-  }
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
 
-  private config(): void {
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(cors());
-    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-  }
+    private config(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(cors());
+        this.app.use(
+            "/api-docs",
+            swaggerUi.serve,
+            swaggerUi.setup(swaggerDocs)
+        );
+    }
 
-  routes() {
-    this.app.use("/", (req, res) => {
-      return res.json({ message: "Hello World!" });
-    });
-  }
+    routes() {
+        this.app.use("/", (req, res) => {
+            return res.json({ message: "Hello World!" });
+        });
+    }
 }
 
 export default new App().app;
