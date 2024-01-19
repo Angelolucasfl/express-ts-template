6f5035b271c641d7f3c6e0858263ce34fc6ed3c5
Angelo Lucas <Angelolucasfl@gmail.com>
2024-01-19 20:38:25 -0300

    final commit

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ dotenv.config();
 const port = Number(process.env.PORT);
 
 app.listen(port, () => {
-    console.log(`Server runing on http://127.0.0.1:${port}/api-docs`);
+  console.log(`Server runing on http://127.0.0.1:${port}/api-docs`);
 });
